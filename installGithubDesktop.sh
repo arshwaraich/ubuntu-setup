0522c1e62afab3a1cacaca1652827478d01e8ff4
arshwaraich12@gmail.com <arshwaraich12@gmail.com>
2019-11-22 01:45:49 +0000

    fixed github desktop install

--- installGithubDesktop.sh
+++ installGithubDesktop.sh
@@ -1,1 +1,4 @@
-sudo snap install github-desktop --beta --classic
+cd ~
+wget https://github.com/shiftkey/desktop/releases/download/release-2.0.4-linux1/GitHubDesktop-linux-2.0.4-linux1.snap
+snap remove github-desktop
+snap install GitHubDesktop-linux-2.0.4-linux1.snap --classic --dangerous
